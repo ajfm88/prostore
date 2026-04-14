d9ddf4e1cb008e34a0eb404a25fcc5c111b1ad0f
ａｊｆｍ８８ <ale@ajfm88.com>
2026-04-14 13:50:59 -0700

    86. User Layout & Menu.

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -38,21 +38,23 @@ const UserButton = async () => {
         <DropdownMenuContent className="w-56" align="end" forceMount>
           <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">
-                {session.user?.name}
-              </p>
-              <p className="text-xs leading-none text-muted-foreground">
-                {session.user?.email}
-              </p>
+              <p className="text-sm font-medium leading-none">{session.user?.name}</p>
+              <p className="text-xs leading-none text-muted-foreground">{session.user?.email}</p>
             </div>
           </DropdownMenuLabel>
-
+          <DropdownMenuItem>
+            <Link className="w-full" href="/user/profile">
+              User Profile
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link className="w-full" href="/user/orders">
+              Order History
+            </Link>
+          </DropdownMenuItem>
           <DropdownMenuItem className="p-0 mb-1">
             <form action={signOut} className="w-full">
-              <Button
-                className="w-full py-4 px-2 h-4 justify-start"
-                variant="ghost"
-              >
+              <Button className="w-full py-4 px-2 h-4 justify-start" variant="ghost">
                 Sign Out
               </Button>
             </form>
