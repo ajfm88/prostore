e06d2ef5c6e5f0e4952da4c9d60e5461912f4d87
ａｊｆｍ８８ <ale@ajfm88.com>
2026-08-08 10:32:57 -0700

    158. Icon Boxes Component.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,26 @@
 import ProductList from "@/components/shared/product/product-list";
-import { getFeaturedProducts, getLatestProducts } from "@/lib/actions/product.actions";
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from "@/lib/actions/product.actions";
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ViewAllProductsButton from "@/components/view-all-products-button";
+import IconBoxes from "@/components/icon-boxes";
 
-const HomePage = async () => {
+const Homepage = async () => {
   const latestProducts = await getLatestProducts();
   const featuredProducts = await getFeaturedProducts();
 
   return (
-    <div className="space-y-8">
-      {featuredProducts.length > 0 && <ProductCarousel data={featuredProducts} />}
+    <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
       <ViewAllProductsButton />
-    </div>
+      <IconBoxes />
+    </>
   );
 };
 
-export default HomePage;
+export default Homepage;
